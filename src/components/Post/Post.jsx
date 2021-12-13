7f15ac82ea79fe3dbab7a422760b03b65c2c9fe4
Syed Shahid <syedshahid1215@gmail.com>
2021-12-13 21:08:14 +0500

    complete Post component

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,9 +1,34 @@
 import './Post.css';
-
+import { MoreVert } from "@mui/icons-material";
 function Post() {
     return (
         <div className='post'>
-            post
+            <div className="postWrapper">
+                <div className="postTop">
+                    <div className="postTopLeft">
+                        <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
+                        <span className="postUsername">Safak Kocaoglu</span>
+                        <span className="postDate">5 min ago</span>
+                    </div>
+                    <div className="postTopRight">
+                        <MoreVert />
+                    </div>
+                </div>
+                <div className="postCenter">
+                    <span className="postText">Hey! Its my first post !</span>
+                    <img className="postImg" src="/assets/post/1.jpeg" alt="" />
+                </div>
+                <div className="postBottom">
+                    <div className="postBottomLeft">
+                        <img className="likeIcon" src="/assets/like.png" alt="" />
+                        <img className="likeIcon" src="/assets/heart.png" alt="" />
+                        <span className="postLikeCounter">32 people liked it</span>
+                    </div>
+                    <div className="postBottomRight">
+                        <span className="postCommentText">9 comments</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
