4c55bc25c864e064b43c6e5866a7d26209e6de87
Syed Shahid <syedshahid1215@gmail.com>
2021-12-13 16:17:22 +0500

    add Home page in pages dir and place TopBar in Home instead directly into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import TopBar from "./components/TopBar/TopBar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div>
-      <TopBar />
+      <Home />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+import TopBar from "../../components/TopBar/TopBar";
+
+function Home() {
+    return (
+        <div>
+            <TopBar />
+        </div>
+    )
+};
+
+export default Home;
