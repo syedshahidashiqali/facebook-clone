a38c2d7fbeda36138c416a86a3e7e0bf54ac0808
Syed Shahid <syedshahid1215@gmail.com>
2021-12-13 20:32:30 +0500

    add Post component file and call it in Feed component

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,11 +1,13 @@
 import "./Feed.css";
 import Share from "../Share/Share";
+import Post from "../Post/Post";
 
 function Feed() {
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share />
+                <Post />
             </div>
         </div>
     )
---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,11 @@
+import './Post.css';
+
+function Post() {
+    return (
+        <div className='post'>
+            post
+        </div>
+    );
+};
+
+export default Post;
