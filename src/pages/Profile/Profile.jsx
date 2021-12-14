128570d68bd9e81dc56e485e2531f8e4566ee8d0
Syed Shahid <syedshahid1215@gmail.com>
2021-12-14 15:42:50 +0500

    complete profile page but sidebar in profile page is remaining

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,32 @@
 import "./Profile.css";
+import TopBar from "../../components/TopBar/TopBar";
+import SideBar from "../../components/SideBar/SideBar";
+import Feed from "../../components/Feed/Feed";
+import RightBar from "../../components/RightBar/RightBar";
 
 function Profile() {
     return (
         <div>
-            Profile
+            <TopBar />
+            <div className="profile">
+                <SideBar />
+                <div className="profileRight">
+                    <div className="profileRightTop">
+                        <div className="profileCover">
+                            <img className="profileCoverImg" src="/assets/post/3.jpeg" alt="" />
+                            <img className="profileUserImg" src="/assets/person/7.jpeg" alt="" />
+                        </div>
+                        <div className="profileInfo">
+                            <h4 className="profileInfoName">Safak Kocaoglu</h4>
+                            <span className="profileInfoDesc">Hello my friends!</span>
+                        </div>
+                    </div>
+                    <div className="profileRightBottom">
+                        <Feed />
+                        <RightBar />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
