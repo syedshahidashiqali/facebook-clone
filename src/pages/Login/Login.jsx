a2737a5f23ab7966ed33f80097cc06f0f4979074
Syed Shahid <syedshahid1215@gmail.com>
2021-12-14 18:53:57 +0500

    complete login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,7 +3,23 @@ import "./Login.css";
 function Login() {
     return (
         <div className="login">
-            Login page
+            <div className="loginWrapper">
+                <div className="loginLeft">
+                    <h3 className="loginLogo">Facebook</h3>
+                    <span className="loginDesc">
+                        Connect with friends and the world around you on Facebook.
+                    </span>
+                </div>
+                <div className="loginRight">
+                    <div className="loginBox">
+                        <input className="loginInput" type="email" placeholder="Email" />
+                        <input className="loginInput" type="password" placeholder="Password" />
+                        <button className="loginButton">Log In</button>
+                        <span className="loginForgot">Forgot Password?</span>
+                        <button className="loginRegisterButton">Create a New Account</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
