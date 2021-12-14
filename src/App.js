1afc84343427578660fa15773c66a603bf248995
Syed Shahid <syedshahid1215@gmail.com>
2021-12-14 17:24:37 +0500

    add Login page file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import Home from "./pages/Home/Home";
 
-import Profile from "./pages/Profile/Profile";
+// import Profile from "./pages/Profile/Profile";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div>
       {/* <Home /> */}
-      <Profile />
+      {/* <Profile /> */}
+      <Login />
     </div>
   );
 };
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+import "./Login.css";
+
+function Login() {
+    return (
+        <div className="login">
+            Login page
+        </div>
+    );
+};
+
+export default Login;
