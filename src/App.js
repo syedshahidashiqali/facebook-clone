96fdfdbfc7323f852da78edacb913cde3dfb62f8
Syed Shahid <syedshahid1215@gmail.com>
2021-12-15 14:20:21 +0500

    add react router to App.js but pics are not showing

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import Profile from "./pages/Profile/Profile";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 
+// import react router things
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      {/* <Home /> */}
-      <Profile />
-      {/* <Login /> */}
-      {/* <Register /> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route  path="/" element={<Home />} />
+        <Route  path="/profile/:username" element={<Profile />} />
+        <Route  path="/login" element={<Login />} />
+        <Route  path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
