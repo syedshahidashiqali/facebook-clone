08cff7047dd9d3f5ce909e7380ac152d97517641
Syed Shahid <syedshahid1215@gmail.com>
2021-12-15 19:14:11 +0500

    make timeline feef dynamic by fetching data from the backend

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -2,10 +2,10 @@ import './Post.css';
 import { MoreVert } from "@mui/icons-material";
 import { useState, useEffect } from "react";
 import axios from 'axios';
+import { format } from "timeago.js";
 
 
 function Post({ post }) {
-    console.log(post)
 
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
     // post matching user logic
@@ -28,7 +28,8 @@ function Post({ post }) {
 
         };
         fetchUser();
-    },[]);
+    },[post.userId]);
+    
     return (
         <div className='post'>
             <div className="postWrapper">
@@ -36,7 +37,7 @@ function Post({ post }) {
                     <div className="postTopLeft">
                         <img className="postProfileImg" src={user.profilePicture || PF+"/person/noAvatar.png"} alt="" />
                         <span className="postUsername">{user.username}</span>
-                        <span className="postDate">{post.date}</span>
+                        <span className="postDate">{format(post.createdAt)}</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVert />
@@ -44,7 +45,7 @@ function Post({ post }) {
                 </div>
                 <div className="postCenter">
                     <span className="postText">{post?.description}</span>
-                    <img className="postImg" src={PF+post.image} alt="" />
+                    <img className="postImg" src={PF+post?.image} alt="" />
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
