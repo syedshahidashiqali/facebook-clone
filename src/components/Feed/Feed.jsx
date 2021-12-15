eae816e1ed3d437dbc7a728b83cfbf9003ff8374
Syed Shahid <syedshahid1215@gmail.com>
2021-12-15 18:23:17 +0500

    remove proxy from package.json

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,16 +1,27 @@
 import "./Feed.css";
 import Share from "../Share/Share";
 import Post from "../Post/Post";
-import { Posts } from "../../dummyData";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Feed() {
+
+    const [ posts, setPosts ] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async() => {
+            const res = await axios.get("http://localhost:5000/api/v1/posts/timeline/61b5f26629cf719d838f08e3");
+            console.log(res);
+        };
+        fetchPosts();
+    },[])
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share />
-                {Posts.map((post) => (
+                {/* {Posts.map((post) => (
                     <Post key={post.id} post={post} />
-                ))}
+                ))} */}
             </div>
         </div>
     )
