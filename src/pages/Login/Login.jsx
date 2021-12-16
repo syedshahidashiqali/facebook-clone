795ba2b999f91365df604f7b87ea2625663801be
Syed Shahid <syedshahid1215@gmail.com>
2021-12-16 16:10:22 +0500

    add form tag in login page and modified both inputs i.e added some fields to it and add ref to these

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,18 @@
 import "./Login.css";
 
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 function Login() {
+
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const submitHandler = e => {
+        e.preventDefault();
+        console.log(13, emailRef.current.value);
+        console.log(14, passwordRef.current.value);
+    };
     return (
         <div className="login">
             <div className="loginWrapper">
@@ -13,15 +23,28 @@ function Login() {
                     </span>
                 </div>
                 <div className="loginRight">
-                    <div className="loginBox">
-                        <input className="loginInput" type="email" placeholder="Email" />
-                        <input className="loginInput" type="password" placeholder="Password" />
+                    <form onSubmit={submitHandler} className="loginBox">
+                        <input 
+                            className="loginInput" 
+                            type="email" 
+                            placeholder="Email"
+                            required 
+                            ref={emailRef}
+                        />
+                        <input 
+                            className="loginInput" 
+                            type="password" 
+                            placeholder="Password" 
+                            required 
+                            ref={passwordRef}
+                            minLength={6}
+                        />
                         <button className="loginButton">Log In</button>
                         <span className="loginForgot">Forgot Password?</span>
                         <Link to="/register" style={{ textDecoration: "none", textAlign:"center" }}>
                             <button className="loginRegisterButton">Create a New Account</button>
                         </Link>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
