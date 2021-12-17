304c093730fff218a9ca4aa29e0e666ee2853c9b
Syed Shahid <syedshahid1215@gmail.com>
2021-12-17 13:43:12 +0500

    add register functionality in Register.jsx

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,37 @@
 import "./Register.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useRef } from "react";
+import axios from "axios";
 
 function Register() {
+
+    const usernameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordAgainRef = useRef();
+    let navigate = useNavigate();
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const url = "http://localhost:5000/api/v1";
+        if(passwordRef.current.value !== passwordAgainRef.current.value){
+            passwordAgainRef.current.setCustomValidity("Password don't match!")
+        } else{
+            const user = {
+                username: usernameRef.current.value,
+                email: emailRef.current.value,
+                password: passwordRef.current.value,
+            };
+
+            try{
+                await axios.post(`${url}/auth/register`, user);
+                navigate("/login", {replace: true});
+            } catch(err){
+                console.log(err);
+            }
+        }
+    };
+
     return (
         <div className="signup">
             <div className="signupWrapper">
@@ -12,16 +42,47 @@ function Register() {
                     </span>
                 </div>
                 <div className="signupRight">
-                    <div className="signupBox">
-                        <input className="signupInput" type="text" placeholder="Username" />
-                        <input className="signupInput" type="email" placeholder="Email" />
-                        <input className="signupInput" type="password" placeholder="Password" />
-                        <input className="signupInput" type="password" placeholder="Password Again" />
-                        <button className="signupButton">Sign Up</button>
+                    <form onSubmit={submitHandler} className="signupBox">
+                        <input 
+                            className="signupInput" 
+                            type="text" 
+                            placeholder="Username" 
+                            ref={usernameRef}
+                            required
+                        />
+                        <input 
+                            className="signupInput" 
+                            type="email" 
+                            placeholder="Email" 
+                            ref={emailRef}
+                            required
+                        />
+                        <input 
+                            className="signupInput" 
+                            type="password" 
+                            placeholder="Password" 
+                            ref={passwordRef}
+                            minLength={6}
+                            required
+                        />
+                        <input 
+                            className="signupInput" 
+                            type="password" 
+                            placeholder="Password Again" 
+                            ref={passwordAgainRef}
+                            minLength={6}
+                            required
+                        />
+                        <button 
+                            type="submit"
+                            className="signupButton"
+                        >
+                            Sign Up
+                        </button>
                         <Link to="/login" style={{ textDecoration: "none", textAlign:"center" }}>
                             <button className="signupLoginButton">Log into an Account</button>
                         </Link>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
