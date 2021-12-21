1c9641c42ffaa3fcc4efe24461b15abd436921d8
Syed Shahid <syedshahid1215@gmail.com>
2021-12-21 17:41:38 +0500

    add two actions in AuthActions.js

--- src/context/AuthActions.js
+++ src/context/AuthActions.js
@@ -1,13 +1,23 @@
-const LoginStart = (userCredentials) => ({
+export const LoginStart = (userCredentials) => ({
   type: "LOGIN_START",
 });
 
-const LoginSuccess = (user) => ({
+export const LoginSuccess = (user) => ({
   type: "LOGIN_SUCCESS",
   payload: user,
 });
 
-const LoginFailure = (error) => ({
+export const LoginFailure = (error) => ({
   type: "LOGIN_FAILURE",
   payload: error,
+});
+
+export const Follow = (userId) => ({
+  type: "FOLLOW",
+  payload: userId,
+});
+
+export const Unfollow = (userId) => ({
+  type: "UNFOLLOW",
+  payload: userId,
 });
