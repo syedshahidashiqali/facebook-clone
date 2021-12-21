73fffcdeb71987b8410f0b6049a55f5cc9202672
Syed Shahid <syedshahid1215@gmail.com>
2021-12-21 19:07:40 +0500

    added local storage so that we dont have to login again and again

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,8 +1,8 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import AuthReducer from "./AuthReducer";
 
 const initialState = {
-  user: null,
+  user: JSON.parse(localStorage.getItem("user")) || null,
   isFetching: false,
   error: false,
 };
@@ -12,6 +12,9 @@ export const AuthContext = createContext(initialState);
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, initialState);
 
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user))
+  }, [state?.user]);
   return (
     <AuthContext.Provider
       value={{
